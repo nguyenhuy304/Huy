383dd0744d0b5891c9da0d4b4e97168d77cb77b5
nguyenhuy304 <53434483+nguyenhuy304@users.noreply.github.com>
2019-12-23 16:43:24 +0700

    doan
    whatsup

---
+++ whatsup.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+	<?php include "../helper/bootstrap_common.php";?>
+ </head>
+
+ <?php include "../asset/css/navstyle.php";?>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>Title</title>
+    <STYLE>
+        .title{
+            font-family: Algerian;
+            font-size: 100px;
+            padding: 20px;
+            text-align: center;
+            color: red;
+        }
+        .voucher{
+            font-family: "Agency FB";
+            font-size: 50px;
+            padding: 20px;
+            color: red;
+            text-align: center;
+                }
+        .event{
+            font-family: Aharoni;
+            font-size: 30px;
+        }
+    </STYLE>
+ <body> 
+    <?php include "../setting/constant.php";?>   
+    <?php $Detect = EXP ;?>
+    <div><?php include "../helper/header_common.php";?></div>
+    <div class="title">
+    UPCOMING EVENT
+</div>
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="https://www.wrs.com.sg/content/dam/wrs/whats-on/LUMINA_S2_900x600.png.transform/compress/resize465x348/img.jpg" data-holder-rendered="true">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="event">Rainforest Lumina Season 2</div>
+NOW TILL 9 FEB 2020
+SINGAPORE ZOO
+Rainforest Lumina is back with a second season! Hear the whispers of the rainforest and witness it come to life. At night fall, the rainforest magically transforms into an enchanted world of lights, virtual animals and immersive play.
+
+Come and discover your inner Creature Crew at Singapore Zoo.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="https://i.pinimg.com/236x/bc/36/79/bc36795b9c8e793f8491c169dc3a3749--animal-posters-event-posters.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="https://www.wrs.com.sg/content/dam/wrs/singapore-zoo/activity/houbii-rope-course-tn.jpg.transform/compress/resize465x348/img.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="https://www.wrs.com.sg/content/dam/wrs/singapore-zoo/campagin/szr2020/Website_WhatsOn_900x600small.jpg.transform/compress/resize465x348/img.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <p class="card-text">
+                    <div class="event">Safari Zoo Run 2020: Final Edition</div>
+                        29 MARCH 2020, SUNDAY
+                        SINGAPORE ZOO, NIGHT SAFARI, RIVER SAFARI
+                        Safari Zoo Run is back for its 12th and final edition! We are taking a break after this edition to make way for the Mandai Rejuvenation Project. Seize this opportunity, choose your team and run for wildlife! It’s the only run in Singapore where you can race or walk alongside Elephants and Giraffes with 4 race categories available!
+
+                        Early bird registration now open.
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="https://www.wrs.com.sg/content/dam/wrs/singapore-zoo/campagin/megafauna/SZ-Main-Website_900pxWX600pxH.jpg.transform/compress/resize465x348/img.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <p class="card-text">
+                    <div class="event">A Mammoth Adventure Back in Time</div>
+                        16 Nov – 29 Dec 2019 Singapore Zoo
+                        Make way for the GIANTS!
+
+                        Travel back in time this school holidays at Singapore Zoo. Stomp the grounds with over 15 life-like prehistoric giants like the Woolly Mammoth, Saber-toothed Tiger and more! Be in awe at the magnitude of their size while you snap photos with a caveman and help a baby mammoth find her mom. An array of fun filled activities awaits you!
+
+                        What’s more! Local residents enjoy up to 20% off admission.
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="https://i.pinimg.com/originals/0b/23/1e/0b231e532fa0bf31c9cb7677132da5d9.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="voucher">
+            VOUCHER
+        </div>
+       <div class="row">
+        <div class="col-md-6">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="https://i.pinimg.com/originals/0b/23/1e/0b231e532fa0bf31c9cb7677132da5d9.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="https://i.pinimg.com/originals/0b/23/1e/0b231e532fa0bf31c9cb7677132da5d9.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">FIND OUT MORE</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
+
+
+
+
+    </div>
+</div>
+    
+   
+
+    <?php include "../helper/footer_common.php";?>
+    <?php include "../helper/script.php";?>
+</body>
+</html>
